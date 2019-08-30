af83164e7764245d5851e56c3399b0ab7568c1d0
Matt Josse <matt.josse@drip.com>
2019-08-30 12:33:06 -0600

    cleanup initial commit

--- cmd/main.go
+++ cmd/main.go
@@ -9,7 +9,7 @@ import (
 	"github.com/mjosc/sqs/pkg/components"
 )
 
-var queueURL = "https://sqs.us-east-1.amazonaws.com/339680549649/TIER-1-SERVICES-TEST-QUEUE-1"
+var queueURL = "some_url_here"
 
 func main() {
 	sess, err := session.NewSession(&aws.Config{
@@ -20,12 +20,12 @@ func main() {
 	}
 
 	client := client.New(sess)
-	// producer := components.NewProducer(1, client, queueURL)
+	producer := components.NewProducer(1, client, queueURL)
 	consumer := components.NewConsumer(1, client, queueURL)
 
-	// if err = producer.Produce("Hello, World!"); err != nil {
-	// 	log.Fatalf("produce error: %v", err)
-	// }
+	if err = producer.Produce("Hello, World!"); err != nil {
+		log.Fatalf("produce error: %v", err)
+	}
 
 	if err = consumer.Consume(); err != nil {
 		log.Fatalf("consume error: %v", err)
